d8ee1367ea765f464af0f429f0ab693ba1c8aa97
Isaac Hong <isaach3@uci.edu>
2023-08-22 14:59:25 +0900

    collecting all direct info + urls implemented processpoolexec

--- log_parser.py
+++ log_parser.py
@@ -1,8 +1,15 @@
 import csv
 import os
 import time 
+import re
+import sys
+from ipaddress import ip_address
+from datetime import datetime
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import cpu_count
 
-filename = 'access_log.20220801'
+
+# filename = 'access_log.20220801'
 
 def reader(filename):
     with open(filename,'r') as f:
@@ -11,9 +18,9 @@ def reader(filename):
     return log
         
 def save_to_csv(lst,filename):
-    keys = ['IP','Remote Log Name','User Identifier','Datetime','Request','Requested URL','Status Code','Size','Referrer','User Agent','Unknown Client ID']
+    keys = ['IP','Remote Log Name','User Identifier','Timestamp','Method','Path','URL','Version','Status','Bytes','Referrer','User Agent']
     try:
-        with open(f'{filename}.csv','a',encoding='utf-8') as f:
+        with open(f'{filename}.csv','a',encoding='utf-8-sig') as f:
             dict_writer = csv.DictWriter(f,keys,lineterminator='\n')
             dict_writer.writerows(lst)
     except Exception as e:
@@ -24,54 +31,53 @@ def parse(log,filename):
     lst = []
     for line in log:
         if line:
-            try:
-                line = line.split(' ')
-                if 'unknown' in line[0]:
-                    unknown = True
-                    line = line[1:]
-                else:
-                    unknown = False
-                while line[0][-1] == ',':
-                    line[1] = line[0]+line[1]
-                    line = line[1:]
-                ip = line[0]
-                identd = line[1]
-                rehg = line[2]
-                datetime = line[3] + line[4]
-                request = ''.join([line[5],line[6]]) + ' ' + line[7]
-                requested_url = 'http://www.lafent.com' + request[request.find('/'):request.find(line[7])]
-                requested_url = requested_url.strip()
-                status = line[8]
-                size = line[9]
-                referrer = line[10]
-                user_agent = ''.join([l for l in line[11:]])
+            line = line.strip('\'')
+            pattern = r'^(?P<ips>(?:(?:\S+)(?:,\s*\S+)*)) (?P<identd>\S+) (?P<user>\S+) \[(?P<timestamp>[^\]]+)\] "(?P<method>\S+) (?P<path>\S+)(?: (?P<version>\S+))?" (?P<status>\d+) (?P<bytes>\d+) "(?P<referrer>.*?)" "(?P<user_agent>.*?)"'
+            match = re.match(pattern,line)
+            if match:
+                ips = [ip.strip() for ip in match.group("ips").split(",")]
+                identd = match.group("identd")
+                user = match.group("user")
+                timestamp = datetime.strptime(match.group("timestamp"),'%d/%b/%Y:%H:%M:%S %z')
+                method = match.group("method")
+                path = match.group("path")
+                url = f'https://www.lafent.com{path}'
+                version = match.group("version")
+                status = match.group("status")
+                bytes = match.group("bytes")
+                referrer = match.group("referrer")
+                user_agent = match.group("user_agent")
                 lst.append({
-                    'IP':ip,
+                    'IP':ips,
                     'Remote Log Name':identd,
-                    'User Identifier':rehg,
-                    'Datetime':datetime,
-                    'Request':request,
-                    'Requested URL':requested_url,
-                    'Status Code':status,
-                    'Size':size,
-                    'Referrer':referrer,
-                    'User Agent':user_agent,
-                    'Unknown Client ID':unknown
+                    'User Identifier':user,
+                    'Timestamp':timestamp,
+                    'Method':method,
+                    "Path":path,
+                    "URL":url,
+                    "Version":version,
+                    "Status":status,
+                    "Bytes":bytes,
+                    "Referrer":referrer,
+                    "User Agent":user_agent
                 })
-            except Exception as e:
-                print(line)
-                print(e)
+            else:
+                with open('invalid_paths.txt','a',encoding='utf-8-sig') as sys.stdout:
+                    print(line)
     save_to_csv(lst,filename)
 
 def init_dir(filename):
-    keys = ['IP','Remote Log Name','User Identifier','Datetime','Request','Requested URL','Status Code','Size','Referrer','User Agent','Unknown Client ID']
-    with open(f'{filename}.csv','w',encoding='utf-8') as f:
+    keys = ['IP','Remote Log Name','User Identifier','Timestamp','Method','Path','URL','Version','Status','Bytes','Referrer','User Agent']
+    with open(f'{filename}.csv','w',encoding='utf-8-sig') as f:
         dict_writer = csv.DictWriter(f,keys)
         dict_writer.writeheader()
 
+def process_log_file(f):
+    log = reader(f)
+    init_dir(f)
+    parse(log,f)
+
 if __name__ == '__main__':
-    for f in os.listdir(os.getcwd()):
-        if str(f).startswith('access_log'):
-            log = reader(f)
-            init_dir(f)
-            parse(log,f)
+    with ProcessPoolExecutor(max_workers=cpu_count()-3) as executor:
+        log_files = [f for f in os.listdir() if str(f).startswith('access_log')]
+        executor.map(process_log_file, log_files)
